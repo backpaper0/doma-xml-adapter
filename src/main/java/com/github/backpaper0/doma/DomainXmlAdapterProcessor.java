f79310330bd67c0ffffa86503acfb306129fb26e
Taichi Uragami <backpaper0@gmail.com>
2016-02-09 22:24:39 +0900

    XmlAdapterサブクラス生成処理を別メソッドに切り出した

--- src/main/java/com/github/backpaper0/doma/DomainXmlAdapterProcessor.java
+++ src/main/java/com/github/backpaper0/doma/DomainXmlAdapterProcessor.java
@@ -2,6 +2,7 @@ package com.github.backpaper0.doma;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.Writer;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
@@ -21,36 +22,13 @@ public class DomainXmlAdapterProcessor extends AbstractProcessor {
         for (TypeElement typeElement : annotations) {
             for (TypeElement domainClassElement : ElementFilter
                     .typesIn(roundEnv.getElementsAnnotatedWith(typeElement))) {
-
                 DomainXmlAdapterModel model = new DomainXmlAdapterModel(domainClassElement,
                         processingEnv.getElementUtils());
-
                 try {
                     JavaFileObject file = processingEnv.getFiler().createSourceFile(
                             model.getPackageName() + "." + model.getAdapterClassSimpleName());
-                    try (PrintWriter out = new PrintWriter(file.openWriter())) {
-                        out.printf("package %1$s;%n", model.getPackageName());
-                        out.printf("%n");
-                        out.printf("public class %1$s extends %2$s<%3$s, %4$s> {%n",
-                                model.getAdapterClassSimpleName(), XmlAdapter.class.getName(),
-                                model.getValueType(), model.getBoundType());
-                        out.printf("%n");
-                        out.printf("    public %1$s unmarshal(%2$s v) throws Exception {%n",
-                                model.getBoundType(), model.getValueType());
-                        if (model.getFactoryMethod().equals("new")) {
-                            out.printf("        return new %1$s(v);%n", model.getBoundType());
-                        } else {
-                            out.printf("        return %1$s.%2$s(v);%n", model.getBoundType(),
-                                    model.getFactoryMethod());
-                        }
-                        out.printf("    }%n");
-                        out.printf("%n");
-                        out.printf("    public %1$s marshal(%2$s v) throws Exception {%n",
-                                model.getValueType(), model.getBoundType());
-                        out.printf("        return v.%1$s();%n", model.getAccessorMethod());
-                        out.printf("    }%n");
-                        out.printf("}%n");
-                        out.flush();
+                    try (Writer writer = file.openWriter()) {
+                        generate(model, writer);
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -58,6 +36,32 @@ public class DomainXmlAdapterProcessor extends AbstractProcessor {
             }
         }
         return false;
+    }
+
+    private static void generate(DomainXmlAdapterModel model, Writer writer) {
+        PrintWriter out = new PrintWriter(writer);
+        out.printf("package %1$s;%n", model.getPackageName());
+        out.printf("%n");
+        out.printf("public class %1$s extends %2$s<%3$s, %4$s> {%n",
+                model.getAdapterClassSimpleName(), XmlAdapter.class.getName(), model.getValueType(),
+                model.getBoundType());
+        out.printf("%n");
+        out.printf("    public %1$s unmarshal(%2$s v) throws Exception {%n", model.getBoundType(),
+                model.getValueType());
+        if (model.getFactoryMethod().equals("new")) {
+            out.printf("        return new %1$s(v);%n", model.getBoundType());
+        } else {
+            out.printf("        return %1$s.%2$s(v);%n", model.getBoundType(),
+                    model.getFactoryMethod());
+        }
+        out.printf("    }%n");
+        out.printf("%n");
+        out.printf("    public %1$s marshal(%2$s v) throws Exception {%n", model.getValueType(),
+                model.getBoundType());
+        out.printf("        return v.%1$s();%n", model.getAccessorMethod());
+        out.printf("    }%n");
+        out.printf("}%n");
+        out.flush();
     }
 
     @Override
