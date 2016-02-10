5a6883039edc9028d77a30c6d917489356ee9a70
Taichi Uragami <backpaper0@gmail.com>
2016-02-10 20:52:27 +0900

    テストコードを整理

--- src/test/java/com/github/backpaper0/doma/DomainXmlAdapterProcessorTest.java
+++ src/test/java/com/github/backpaper0/doma/DomainXmlAdapterProcessorTest.java
@@ -1,7 +1,11 @@
 package com.github.backpaper0.doma;
 
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+
+import javax.tools.JavaFileObject;
 
 import org.junit.Test;
 
@@ -11,50 +15,43 @@ import com.google.testing.compile.JavaSourceSubjectFactory;
 
 public class DomainXmlAdapterProcessorTest {
 
+    private final DomainXmlAdapterProcessor processor = new DomainXmlAdapterProcessor();
+
     @Test
     public void test() throws Exception {
         Truth.ASSERT.about(JavaSourceSubjectFactory.javaSource())
-                .that(JavaFileObjects.forSourceLines("com.example.Key",
-                        Files.readAllLines(Paths.get(getClass().getResource("/Key.txt").toURI()))))
-                .processedWith(new DomainXmlAdapterProcessor()).compilesWithoutError().and()
-                .generatesSources(
-                        JavaFileObjects.forSourceLines("com.example.KeyAdapter", Files.readAllLines(
-                                Paths.get(getClass().getResource("/KeyAdapter.txt").toURI()))));
+                .that(source("com.example.Key", "/Key.txt")).processedWith(processor)
+                .compilesWithoutError().and()
+                .generatesSources(source("com.example.KeyAdapter", "/KeyAdapter.txt"));
     }
 
     @Test
     public void factoryMethod() throws Exception {
         Truth.ASSERT.about(JavaSourceSubjectFactory.javaSource())
-                .that(JavaFileObjects.forSourceLines("com.example.Key",
-                        Files.readAllLines(Paths
-                                .get(getClass().getResource("/Key_factoryMethod.txt").toURI()))))
-                .processedWith(new DomainXmlAdapterProcessor()).compilesWithoutError().and()
-                .generatesSources(JavaFileObjects.forSourceLines("com.example.KeyAdapter",
-                        Files.readAllLines(Paths.get(
-                                getClass().getResource("/KeyAdapter_factoryMethod.txt").toURI()))));
+                .that(source("com.example.Key", "/Key_factoryMethod.txt")).processedWith(processor)
+                .compilesWithoutError().and().generatesSources(
+                        source("com.example.KeyAdapter", "/KeyAdapter_factoryMethod.txt"));
     }
 
     @Test
     public void accessorMethod() throws Exception {
         Truth.ASSERT.about(JavaSourceSubjectFactory.javaSource())
-                .that(JavaFileObjects.forSourceLines("com.example.Key",
-                        Files.readAllLines(Paths
-                                .get(getClass().getResource("/Key_accessorMethod.txt").toURI()))))
-                .processedWith(new DomainXmlAdapterProcessor()).compilesWithoutError().and()
-                .generatesSources(JavaFileObjects.forSourceLines("com.example.KeyAdapter",
-                        Files.readAllLines(Paths.get(getClass()
-                                .getResource("/KeyAdapter_accessorMethod.txt").toURI()))));
+                .that(source("com.example.Key", "/Key_accessorMethod.txt")).processedWith(processor)
+                .compilesWithoutError().and().generatesSources(
+                        source("com.example.KeyAdapter", "/KeyAdapter_accessorMethod.txt"));
     }
 
     @Test
     public void generics() throws Exception {
         Truth.ASSERT.about(JavaSourceSubjectFactory.javaSource())
-                .that(JavaFileObjects.forSourceLines("com.example.Key",
-                        Files.readAllLines(
-                                Paths.get(getClass().getResource("/Key_generics.txt").toURI()))))
-                .processedWith(new DomainXmlAdapterProcessor()).compilesWithoutError().and()
-                .generatesSources(JavaFileObjects.forSourceLines("com.example.KeyAdapter",
-                        Files.readAllLines(Paths
-                                .get(getClass().getResource("/KeyAdapter_generics.txt").toURI()))));
+                .that(source("com.example.Key", "/Key_generics.txt")).processedWith(processor)
+                .compilesWithoutError().and()
+                .generatesSources(source("com.example.KeyAdapter", "/KeyAdapter_generics.txt"));
+    }
+
+    private JavaFileObject source(String className, String resource) throws Exception {
+        Path file = Paths.get(getClass().getResource(resource).toURI());
+        List<String> lines = Files.readAllLines(file);
+        return JavaFileObjects.forSourceLines(className, lines);
     }
 }
